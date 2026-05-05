d06f3c5b18ce130a23494696414fd4d32a3fcc91
Sofyane Amrhar <soa016@edu.zealand.dk>
2026-05-05 23:24:10 +0200

    Tilføjer Besøgslog klasse

---
+++ Shelterudlejningssystemet /Besøgslog.cs
@@ -0,0 +1,35 @@
+namespace Shelterudlejningssystemet;
+
+public class Besøgslog
+{
+    private int _logId;
+    private DateTime _startDato;
+    private DateTime _slutDato;
+    private string _tekst = "";
+
+    public int LogId
+    {
+        get => _logId;
+        set => _logId = value;
+    }
+
+    public DateTime StartDato
+    {
+        get => _startDato;
+        set => _startDato = value;
+    }
+
+    public DateTime SlutDato
+    {
+        get => _slutDato;
+        set => _slutDato = value;
+    }
+
+    public string Tekst
+    {
+        get => _tekst;
+        set => _tekst =  value;
+    }
+
+   
+}
